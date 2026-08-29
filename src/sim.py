dc0d94766bafd416ee7d09a8ae3bef51f6bcf2be
Bastian Malte Winter <bastianmaltewinter@Bastians-MacBook-Air.fritz.box>
2026-08-29 19:37:55 +0200

    force

--- src/sim.py
+++ src/sim.py
@@ -1,6 +1,5 @@
 import random
 
-import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -23,30 +22,47 @@ def generate_before(d: int, bead_density: int) -> np.ndarray:
 
 
 # Define a rotational force (vortex-like)
-def force(x: int, y: int, magnitude: float) -> tuple:
+def force(x: int, y: int, magnitude: float, noise_strength) -> tuple:
     """
     Returns a displacement vector (dx, dy) based on position.
     """
 
-    distance = np.sqrt(x**2 + y**2)
+    distance = x**2 + y**2
     if distance == 0:
         return 0, 0
-    dx = -x * magnitude / distance
-    dy = -y * magnitude / distance
-    return int(dx), int(dy)
+    dx = -y * magnitude / distance
+    dy = x * magnitude / distance
+
+    # Add random noise
+    noise_x = random.uniform(-noise_strength, noise_strength)
+    noise_y = random.uniform(-noise_strength, noise_strength)
+    total_dx = dx + noise_x
+    total_dy = dy + noise_y
+
+    return (round(total_dx)), (round(total_dy))
 
 
 # Apply force and move beads
-def generate_after(before, d, magnitude):
-    after = np.copy(before)
+def generate_after(before: np.ndarray, *force_par) -> np.ndarray:
+    """Generates particle field after force application.
 
+    Args:
+        before: Before Image
+        magnitude: Magnitude of force
+        force_par: force parameters
+
+    Returns:
+
+    """
+    after = np.copy(before)
+    d = np.shape(before)[0]
     for row in range(d):
         for col in range(d):
             if before[row][col] == 1:
                 # Relative position to center
                 x = col - d // 2
                 y = row - d // 2
-                dx, dy = force(x, y, magnitude)
+                dx, dy = force(x, y, *force_par)
 
                 new_row = row + dy
                 new_col = col + dx
